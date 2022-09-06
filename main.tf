b5f51dde21b29445bbebc1487e7e8e27284007f8
chakri1311 <112957080+chakri1311@users.noreply.github.com>
2022-09-06 19:19:13 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "ap-southeast-1"
+  access_key = var.access_key
+  secret_key = var.sec_key
+}
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name = "RUShi"
+  count = length(var.instance_id)
+
+  tags = {
+    Name =  var.instance_id[count.index]
+  }
+}
